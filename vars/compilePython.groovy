b27e09cd410bc7c99b31514597105b46243a7e87
Erik Ramfelt <erik.ramfelt@flexlink.com>
2017-05-30 16:08:07 +0200

    Updated

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,8 +1,4 @@
-def call(){
-if (fileExists("requirements.txt")){
-sh "virtualenv venv"
-sh "venv/bin/pip install -r requirements.txt"
-}
-sh """find . -name '*.py' -print0|
+def call(String directory = '.') {
+sh """find ${directory} -name '*.py' -print0|
 xargs -0 -t -L 1 python2 -m py_compile"""
 }
