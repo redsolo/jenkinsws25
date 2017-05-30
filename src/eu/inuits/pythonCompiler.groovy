3a2f2cabe622c62acecb7e35d7ed52c24a4fcb09
Erik Ramfelt <erik.ramfelt@flexlink.com>
2017-05-30 16:24:15 +0200

    Updated

--- src/eu/inuits/pythonCompiler.groovy
+++ src/eu/inuits/pythonCompiler.groovy
@@ -1,8 +1,6 @@
 package eu.inuits
 class PythonCompiler {
-static def compileDirectory(script, directory) {
-script.sh """find ${directory} -name '*.py' \
--print0|
-xargs -0 -t -L 1 python2 -m py_compile"""
-}
+	static def compileDirectory(script, directory) {
+		script.sh """find ${directory} -name '*.py' -print0|xargs -0 -t -L 1 python2 -m py_compile"""
+	}
 }
