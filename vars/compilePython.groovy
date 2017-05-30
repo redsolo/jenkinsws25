f987866e5334921257ce573bc96397ec067bbfbb
Erik Ramfelt <erik.ramfelt@flexlink.com>
2017-05-30 16:23:28 +0200

    Updted

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,4 +1,5 @@
+import static eu.inuits.PythonCompiler.*
 def call(String directory = '.') {
-sh """find ${directory} -name '*.py' -print0|
-xargs -0 -t -L 1 python2 -m py_compile"""
+echo("Compiling ${directory}")
+compileDirectory(this, directory)
 }
